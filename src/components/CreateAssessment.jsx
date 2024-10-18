5687f20908cf032e7ca31a253c48f9eaf9f33f50
Leonel Abanilla <leonelabanilla0@gmail.com>
2024-10-19 01:55:03 +0800

    Updated modal

--- src/components/CreateAssessment.jsx
+++ src/components/CreateAssessment.jsx
@@ -12,34 +12,47 @@ export default function CreateAssessment() {
   return (
     <>
     <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Header closeButton className={`${styles.Header}`}>
+            <Modal.Title className={`${styles.Title}`}>Create Assessment</Modal.Title>
             </Modal.Header>
             <Modal.Body>
             <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="name@example.com"
-                    autoFocus
-                />
+                <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+                >
+                <Form.Label>Assessment Title</Form.Label>
+                <Form.Control type="text" />
                 </Form.Group>
                 <Form.Group
                 className="mb-3"
                 controlId="exampleForm.ControlTextarea1"
                 >
-                <Form.Label>Example textarea</Form.Label>
+                <Form.Label>Problem</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+                >
+                <Form.Label>Sample Output</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+                >
+                <Form.Label>Expected Output</Form.Label>
                 <Form.Control as="textarea" rows={3} />
                 </Form.Group>
             </Form>
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary" onClick={handleClose} className={`${styles.Close}`}>
                 Close
             </Button>
-            <Button variant="primary" onClick={handleClose}>
-                Save Changes
+            <Button variant="primary" onClick={handleClose} className={`${styles.Create}`}>
+                Create
             </Button>
             </Modal.Footer>
         </Modal>
