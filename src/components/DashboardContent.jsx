de9d301a2b0279bae8553b3677e7a84a674c50b9
Franze <diazfranzpeter@gmail.com>
2024-10-19 23:04:41 +0800

    added loading page if verifying

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -14,6 +14,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { Accordion } from 'react-bootstrap';
 import CryptoJS from 'crypto-js';
+import LoadingPage from '../pages/LoadingPage';
 
 export const DashboardContent = () => {
     const navigate = useNavigate();
@@ -21,7 +22,7 @@ export const DashboardContent = () => {
     const userData = localStorage.getItem('userData');
 
     if (!userData) {
-      return <div>Loading...</div>; // You can customize this as needed
+      return <LoadingPage />; // You can customize this as needed
     }
 
     const user = JSON.parse(CryptoJS.AES.decrypt(userData, 'capstone').toString(CryptoJS.enc.Utf8));
