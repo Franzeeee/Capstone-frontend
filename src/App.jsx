176494d074b944eba978ef2a0081367bf300cfd6
Vincent Cormero <2100816@lnu.edu.ph>
2024-10-28 02:38:14 +0800

    Added Calendar Page and NewEventForm for Calendar component using Schedule-X package

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import { Login } from './pages/Login'
-import { Register } from './pages/Register'
-import CodeEditor from './pages/CodeEditor'
-import { Home } from './pages/Home'
-import { Dashboard } from './pages/Dashboard'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import CodeEditor from "./pages/CodeEditor";
+import { Home } from "./pages/Home";
+import { Dashboard } from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Routes providers
-import { ProtectedRoutes } from './routes/ProtectedRoutes'
-import Class from './pages/Teacher/Class'
-import ClassLesson from './pages/Teacher/ClassLesson'
-import LoadingPage from './pages/LoadingPage'
-import Announcement from './pages/Announcement'
-import ClassAssessment from './pages/ClassAssessment'
-import NotFound from './pages/NotFound'
-import Sample from './pages/Sample'
-
+import { ProtectedRoutes } from "./routes/ProtectedRoutes";
+import Class from "./pages/Teacher/Class";
+import ClassLesson from "./pages/Teacher/ClassLesson";
+import LoadingPage from "./pages/LoadingPage";
+import Announcement from "./pages/Announcement";
+import ClassAssessment from "./pages/ClassAssessment";
+import NotFound from "./pages/NotFound";
+import Sample from "./pages/Sample";
+import Calendar from "./pages/Calendar";
 
 export const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-
-        <Route element={<ProtectedRoutes />} >
-            <Route path="/playground" element={<CodeEditor />} />
+      <Routes>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/playground" element={<CodeEditor />} />
         </Route>
-            <Route path="/" element={<Sample />} />
+        <Route path="/" element={<Sample />} />
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/c/:code' element={<Class />} />
-        <Route path='/c/:code/:lesson' element={<ClassLesson />}/>
-        <Route path='/c/:code/a/:assessment' element={<ClassAssessment />} />
-        <Route path='/loader' element={<LoadingPage />}/>
-        <Route path='/announcements' element={<Announcement />} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="/c/:code" element={<Class />} />
+        <Route path="/c/:code/:lesson" element={<ClassLesson />} />
+        <Route path="/c/:code/a/:assessment" element={<ClassAssessment />} />
+        <Route path="/loader" element={<LoadingPage />} />
+        <Route path="/announcements" element={<Announcement />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="*" element={<NotFound />} />
 
-        <Route path='/sample' element={<Sample />}/>
+        <Route path="/sample" element={<Sample />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
