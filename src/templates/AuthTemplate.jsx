45d538156f69f3acfe1ac2cd24c8ff76f81803b3
Leonel Abanilla <leonelabanilla0@gmail.com>
2024-10-31 01:35:03 +0800

    Login Update

--- src/templates/AuthTemplate.jsx
+++ src/templates/AuthTemplate.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../assets/css/templates/auth-template.module.css'
+import LoginPic from '../assets/img/LoginPic.png'
 
 export const AuthTemplate = ({children}) => {
   return (
@@ -7,6 +8,7 @@ export const AuthTemplate = ({children}) => {
         <div className={`row ${styles.row}`}>
             {children}
             <div className={`col-md-6 ${styles.rightPanel}`}>
+              <img src={LoginPic} alt="" className={ `${styles.loginPic}`}/>
             </div>
         </div>
     </div>
