4df4b23b8df9413854bc35dca73a3c0735297092
Leonel Abanilla <leonelabanilla0@gmail.com>
2024-11-13 07:27:36 +0800

    Updated LogicLessons

---
+++ src/pages/LogicLessons/Lesson1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import HomeTemplate from '../../templates/HomeTemplate'
+import { getUserData } from '../../utils/userInformation'
+
+export default function Lesson1() {
+  return (
+    <div>Lesson1</div>
+  )
+}
