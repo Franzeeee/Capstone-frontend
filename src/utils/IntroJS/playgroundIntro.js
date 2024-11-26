75421ab904bafed0370dd04e3036869ea2558071
Leonel Abanilla <leonelabanilla0@gmail.com>
2024-11-27 02:32:13 +0800

    Tutorial

--- src/utils/IntroJS/playgroundIntro.js
+++ src/utils/IntroJS/playgroundIntro.js
@@ -2,28 +2,62 @@ const playgroundIntro = {
     steps: [
       {
         element: '#step1',
-        intro: `
-      <p>Watch this YouTube video!</p>
-      <iframe 
-        width="100%" 
-        height="100%" 
-        src="https://www.youtube.com/embed/kiyi-C7NQrQ?si=cVc-xTHf-r0HUpkM" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-      </iframe>
-    `,
+        intro:'The CodeLab logo reminds you where you are and gives your workspace its identity.',
         tooltipClass: 'customTooltip'
       },
       {
         element: '#step2',
-        intro: 'This is the second step!',
+        intro: 'The Folder Icon opens saved files or starts a new project.',
       },
       {
         element: '#step3',
-        intro: 'Here is the last step!',
+        intro: 'The Save Icon lets you save your code progress to avoid losing your work.',
       },
+      {
+        element: '#step4',
+        intro: 'The Clipboard Icon helps you copy and paste snippets or commands.',
+      },
+      {
+        element: '#step5',
+        intro: 'The Python Icon allows you to select or switch programming languages.',
+      },
+      {
+        element: '#step6',
+        intro: 'This is where you write and edit your code. Line numbers on the side help keep things organized.',
+      },
+      {
+        element: '#step7',
+        intro: 'Runs your code instantly. Clicking this compiles and executes what you’ve written in the editor.',
+      },
+      {
+        element: '#step8',
+        intro: 'The Stdin Inputs panel allows you to enter custom data for programs requiring user input.',
+      },
+      {
+        element: '#step9',
+        intro: 'The Output Panel shows the results of your program or any error messages.',
+      },
+      {
+        element: '#step10',
+        intro: 'The Interactive Toggle enables real-time interaction with your code while it’s running.',
+      },
+      {
+        element: '#step11',
+        intro: 'Tracks how long your code takes to execute after you run it.',
+      },
+      {
+        element: '#step12',
+        intro: 'Takes you to coding practice challenges to improve your skills and test what you’ve learned.',
+      },
+      {
+        element: '#step13',
+        intro: 'Lets you type in questions or coding issues, and CodeLab AI will provide helpful guidance or tips.',
+      },
+      {
+        element: '#step14',
+        intro: 'This button will leave you to coding playground.',
+      },
+     
     ],
   }
 
