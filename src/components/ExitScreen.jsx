49d0f9091411c663261282b8909496b1ff73b35c
Franze <diazfranzpeter@gmail.com>
2024-12-02 12:24:11 +0800

    Swap antiCheat statistics display in AssessmentContent and add console logs in ExitScreen for fullscreen actions

--- src/components/ExitScreen.jsx
+++ src/components/ExitScreen.jsx
@@ -9,8 +9,10 @@ export default function ExitScreen({handleFullscreen, focus, addLeaveFullscreen,
     useEffect(() => {
         if(focus) {
             addLeaveFullscreen();
+            console.log('Added Exit fullscreen')
         }else {
             addAltTab();
+            console.log("Added Change tab")
         }
     }, [])
 
